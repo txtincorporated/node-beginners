31bb2b705379368b5c6396fa20c2f5e1fb5839b6
Nathan P. Keene <npk@txtincorporated.com>
2016-10-23 20:58:56 -0700

    examp05 bug specimen

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,9 +1,11 @@
 function start() {
   console.log('Request handler "start" has been called.');
+  return 'Hello Start';
 };
 
 function upload() {
   console.log('Request handler "upload" has been called');
+  return 'Hello Upload';
 };
 
 exports.start = start;
