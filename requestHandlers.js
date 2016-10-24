044b8f9ceb1ead3c48b42cbede10c271cd7b2082
Nathan P. Keene <npk@txtincorporated.com>
2016-10-23 22:57:18 -0700

    Bug observation complete

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,6 +1,14 @@
+var exec = require('child_process').exec;
+
 function start() {
   console.log('Request handler "start" has been called.');
-  return 'Hello Start';
+  var content = 'empty';
+
+  exec('ls -lah', function(error, stdout, stderr) {
+    content = stdout;
+  });
+
+  return content;
 };
 
 function upload() {
