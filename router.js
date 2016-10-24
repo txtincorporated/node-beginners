172f1208b66e6ce5d7d9a8cabf821aad4ee5a98c
Nathan P. Keene <npk@txtincorporated.com>
2016-10-23 20:46:53 -0700

    examp05 corrections

--- router.js
+++ router.js
@@ -1,7 +1,7 @@
-function route(pathname) {
+function route(handle, pathname) {
   console.log('About to route a request for ' + pathname);
   if(typeof(handle[pathname]) === 'function') {
-    handle[pahtname]();
+    handle[pathname]();
   } else {
     console.log('No request handler found for ' + pathname);
   }
